e3f61ddcb5f9733caea0007b714636dbb126edda
Hussian shaik <hussian.nuevesolutions@gmail.com>
2019-12-05 17:51:38 +0530

    finished sample with react & apollo graphql

---
+++ public/precache-manifest.9ce4ca1379175d90b86de1968233d852.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66f3b325d6431b0c5afb589aa1f3cdd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e70c3c5bbd51126cc35",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "831486b793885041295a",
+    "url": "/static/js/2.98b8f345.chunk.js"
+  },
+  {
+    "revision": "7e70c3c5bbd51126cc35",
+    "url": "/static/js/main.da1a8cc6.chunk.js"
+  },
+  {
+    "revision": "1337678ae3301a2c8a7b",
+    "url": "/static/js/runtime-main.8eac158a.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require('./schema');
+const path = require('path')
+const cors = require('cors');
 
 const app = express();
+
+//Allow cross-origin;
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
 
+app.use(express.static('public'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
